a6575fcb2e34fad60ee0245c29bb229aa6c3c2da
jefferson998 <pazjefferson11216@gmail.com>
2026-05-30 12:08:47 -0500

    test gh-pages

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,17 @@
 const { defineConfig } = require("cypress");
+const { allureCypress } = require("allure-cypress/reporter");
 
 module.exports = defineConfig({
   allowCypressEnv: false,
+  video: true,
+  videoCompression: 32,
 
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      allureCypress(on, config, {
+        resultsDir: "allure-results",
+      });
+      return config;
     },
   },
 });
